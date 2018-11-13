1625db61ddf428837e124779eba7be1e63bbaa33
Donald Bales <don.bales@hs2solutions.com>
2018-11-13 10:23:13 -0700

    section 3, lecture 11.

--- public/script.js
+++ public/script.js
@@ -1,3 +1,6 @@
 new Vue({
-  el: '#app' // assign a css selector string for where Vue can access the DOM
+  el: '#app', // assign a css selector string for where Vue can access the DOM
+  data: {
+    total: 0
+  }           // global data 
 });
