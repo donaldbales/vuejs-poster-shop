119a9e8aaf9188f8f15a899d050644add377f713
Donald Bales <don.bales@hs2solutions.com>
2018-11-13 11:16:14 -0700

    section 4, lecture 17.

--- public/script.js
+++ public/script.js
@@ -6,11 +6,13 @@ new Vue({
       { title: 'Item 1' },
       { title: 'Item 2' },
       { title: 'Item 3' }
-    ]
+    ],
+    cart: []
   },          // global data 
   methods: {
-    addItem: function() {
+    addItem: function(index) {
       this.total += 9.99;
+      console.log(index);
     }
   }
 });
