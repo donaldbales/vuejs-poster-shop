6d5e63311138a93454f1df2824b8b3c96b5fb44d
Donald Bales <don.bales@hs2solutions.com>
2018-11-13 10:41:20 -0700

    section 3, lecture 14.

--- public/script.js
+++ public/script.js
@@ -1,7 +1,12 @@
 new Vue({
   el: '#app', // assign a css selector string for where Vue can access the DOM
   data: {
-    total: 0
+    total: 0,
+    items: [
+      { title: 'Item 1' },
+      { title: 'Item 2' },
+      { title: 'Item 3' }
+    ]
   },          // global data 
   methods: {
     addItem: function() {
